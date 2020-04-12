3d5f514679064c872a9ed0fa0cc8bb5f0cd26c4b
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-04-12 17:47:28 +0300

    Created method which will be create random background color after Taping anywhere.

--- lib/main.dart
+++ lib/main.dart
@@ -5,61 +5,47 @@ import 'dart:math';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-
-  var rnd = new Random();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Random color'),
-          centerTitle: true,
-          backgroundColor: Colors.lightBlue,
-        ),
         body: Center(
-          child: InkWell(
-            onTap: () {
-              print('Clicked');
-            },
-            child: Scaffold(
-              backgroundColor: Color.fromARGB(
-                  255,
-                  rnd.nextInt(255),
-                  rnd.nextInt(255),
-                  rnd.nextInt(255)),
-              body: Center(
-                child: Text('Hey there'),
-              ),
-            ),
-          ),
+          child: RandomColor(),
         ),
       ),
     );
   }
 }
 
-//class MyHomePage extends StatelessWidget {
-//  final String title;
-//
-//  MyHomePage({Key key, this.title}) : super(key: key);
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return GestureDetector(
-//      onTap: () {
-//        Container(
-//          child: Text('Less boring'),
-//          color: Colors.red,
-//          alignment: Alignment.center,
-//          constraints: BoxConstraints.tight(Size.infinite),
-//        );
-//      },
-//    );
-//  }
-//}
+class RandomColor extends StatefulWidget {
+  @override
+  RandomColorState createState() => RandomColorState();
+}
 
-// backgroundColor: Color.fromARGB(
-//            255, rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255)),
+class RandomColorState extends State<RandomColor> {
+  // This widget is the root of your application.
+  Color color;
+  Random random = new Random();
+
+  @override
+  void initState() {
+    super.initState();
+
+    color = Colors.transparent;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      child: InkWell(
+        onTap: () {
+          setState(() {
+            color = Color.fromARGB(255, random.nextInt(255),
+                random.nextInt(255), random.nextInt(255));
+          });
+        },
+      ),
+    );
+  }
+}
